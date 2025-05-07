581bb4f01fef521654c4c242cd11bf5ceb679e8f
Fabricio Apaza Morales <josueapaza01@gmail.com>
2025-05-07 12:14:49 -0500

    chore: Initial Commit Base Project

---
+++ src/main/java/upc/LoremIpsum/UpcPre202501Si6573587LoremIpsumApplication.java
@@ -0,0 +1,13 @@
+package upc.LoremIpsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcPre202501Si6573587LoremIpsumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpcPre202501Si6573587LoremIpsumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/upc/LoremIpsum/UpcPre202501Si6573587LoremIpsumApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.LoremIpsum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcPre202501Si6573587LoremIpsumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
