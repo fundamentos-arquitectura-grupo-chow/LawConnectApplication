eee622bf9670af91d6baba253b84acec30af2624
Gabotw <u202018627@upc.edu.pe>
2025-05-09 22:19:29 -0500

    feat: added acl.

---
+++ src/main/java/upc/LoremIpsum/lawconnectplatform/feeing/interfaces/acl/PaymentContextFacade.java
@@ -0,0 +1,56 @@
+package upc.LoremIpsum.lawconnectplatform.feeing.interfaces.acl;
+
+import upc.LoremIpsum.lawconnectplatform.feeing.domain.model.aggregates.Payment;
+import upc.LoremIpsum.lawconnectplatform.feeing.domain.model.commands.CreatePaymentCommand;
+import upc.LoremIpsum.lawconnectplatform.feeing.domain.model.commands.DeletePaymentCommand;
+import upc.LoremIpsum.lawconnectplatform.feeing.domain.model.queries.GetPaymentByIdQuery;
+import upc.LoremIpsum.lawconnectplatform.feeing.domain.services.PaymentCommandService;
+import upc.LoremIpsum.lawconnectplatform.feeing.domain.services.PaymentQueryService;
+import upc.LoremIpsum.lawconnectplatform.feeing.interfaces.rest.resources.PaymentResource;
+import upc.LoremIpsum.lawconnectplatform.feeing.interfaces.rest.transform.PaymentResourceFromEntityAssembler;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class PaymentContextFacade {
+    private final PaymentCommandService paymentCommandService;
+    private final PaymentQueryService paymentQueryService;
+
+    public PaymentContextFacade(
+            @Lazy PaymentCommandService paymentCommandService,
+            @Lazy PaymentQueryService paymentQueryService) {
+        this.paymentCommandService = paymentCommandService;
+        this.paymentQueryService = paymentQueryService;
+    }
+
+    public Optional<Payment> createPayment(
+            Long consultationId,
+            Long clientId,
+            Double amount,
+            Integer currency
+    ){
+        return paymentCommandService.handle(new CreatePaymentCommand(consultationId, clientId,amount,currency));
+    }
+
+    public Optional<Payment> getPaymentById(Long paymentId){
+        return paymentQueryService.handle(new GetPaymentByIdQuery(paymentId));
+    }
+
+    public void deletePaymentById(Long paymentId){
+        paymentCommandService.handle(new DeletePaymentCommand(paymentId));
+    }
+
+    public List<PaymentResource> createPaymentListResource(
+            List<Payment> payment
+    ){
+        var paymentsResources = payment.stream()
+                .map(PaymentResourceFromEntityAssembler::toResourceFromEntity)
+                .collect(Collectors.toList());
+        return Optional.of(paymentsResources).orElseThrow();
+    }
+
+}
